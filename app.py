3c1f19a269dbea4a13c1692d8dcbb5c9ad37ae7e
Kartik <0912kartik@gmail.com>
2023-07-29 15:32:08 +0530

    updated and addedfiles

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import predict
+from flask import Flask, render_template, request, redirect , url_for
+import pickle
+import numpy as np
+import pandas as pd
+import json
 
 
-answer = predict.predictAnswer(Context, question)
+# answer = predict.predictAnswer(Context, question)
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def main():
+    # if request.method=='POST':
+    #     Context = request.form['Context']
+        
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
